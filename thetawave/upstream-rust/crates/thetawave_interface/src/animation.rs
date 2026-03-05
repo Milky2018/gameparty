dd9fa536f011e8d4da3c0b071280e07b96098d8c
Milky2018 <842376130@qq.com>
2026-03-05 17:40:40 +0800

    chore: remove thetawave upstream-rust mirror

--- thetawave/upstream-rust/crates/thetawave_interface/src/animation.rs
+++
@@ -1,6 +0,0 @@
-use bevy_ecs::{entity::Entity, event::Event};
-
-/// This event is used for notifying systems when an animation for an entity has been completed
-/// Can be used for despawning entities after animations finish
-#[derive(Event)]
-pub struct AnimationCompletedEvent(pub Entity);
--- thetawave/upstream-rust/src/game/counters/mod.rs
+++
@@ -1,2 +0,0 @@
-/// All of the mutations exposed for the singletons in the above module.
-pub mod plugin;
